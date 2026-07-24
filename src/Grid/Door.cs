5c295005d8351e68e1e1a2bb8d5e2aa70ef3f4e4
Tom Rijnbeek <tom@tomrijnbeek.me>
2026-07-24 23:29:30 +0200

    🚪 Add doors and switches

---
+++ src/Grid/Door.cs
@@ -0,0 +1,37 @@
+using Godot;
+using HalfNibbleGame.Data;
+
+namespace HalfNibbleGame.Grid;
+
+public partial class Door : StaticGridObject {
+  // Lazily initialized
+  private Sprite2D sprite = null!;
+
+  private bool open;
+
+  public override void _Ready() {
+    base._Ready();
+    sprite = GetNode<Sprite2D>("Sprite");
+    Level!.RegisterTileModifier(Coords, applyCollisionToTile);
+  }
+
+  public void Open() {
+    open = true;
+
+    // Oh oh so hacky
+    Animations.Animations.DoDelayed(Constants.TimeBetweenRounds * 0.75, () => sprite.Modulate = new Color(sprite.Modulate, 0.5f));
+    Animations.Animations.DoDelayed(Constants.TimeBetweenRounds, () => {
+      sprite.Modulate = new Color(1, 1, 1);
+      sprite.Visible = false;
+    });
+  }
+
+  public void Close() {
+    open = false;
+    sprite.Visible = true;
+  }
+
+  private Tile applyCollisionToTile(Tile tile) {
+    return open ? tile : tile with { Collides = true };
+  }
+}
---
+++ src/Grid/FloorSwitch.cs
@@ -0,0 +1,54 @@
+using System.Linq;
+using Godot;
+using HalfNibbleGame.Adventurers;
+using HalfNibbleGame.Data;
+using HalfNibbleGame.Replay;
+
+namespace HalfNibbleGame.Grid;
+
+public partial class FloorSwitch : StaticGridObject, ISimulated {
+
+  [Export] private Door? door;
+
+  // Lazily initialized
+  private Sprite2D sprite = null!;
+
+  private int pressedRound = int.MaxValue;
+  private bool active;
+
+  public override void _Ready() {
+    base._Ready();
+    AddToGroup(Groups.Simulated);
+    sprite = GetNode<Sprite2D>("Sprite");
+  }
+
+  public void Advance(RoundContext context) {
+    context.RegisterOutcome(() => checkPressed(context.RoundNumber));
+  }
+
+  private void checkPressed(int roundNumber) {
+    // If any adventurer is on this tile after a round, then we break the next round whether the adventurer moves or not
+    // TODO: maybe this should not just check for adventurers
+    if (GetTree().GetNodesInGroup(Groups.Simulated).OfType<Adventurer>().Any(a => a.Coords == Coords)) {
+      pressedRound = roundNumber;
+      // Because we check after the movement of a round, we can safely already activate this switch
+      activate();
+    }
+  }
+
+  public void ResetToRound(int roundNumber) {
+    if (active && roundNumber <= pressedRound) {
+      deactivate();
+    }
+  }
+
+  private void activate() {
+    active = true;
+    door?.Open();
+  }
+
+  private void deactivate() {
+    door?.Close();
+    active = false;
+  }
+}
