9478182ab341388d2fb9c4e5be6cd2e1d03a03c3
William Barbosa <heytherewill@gmail.com>
2026-07-24 22:54:30 +0200

    🗂️ Folder organization

--- export.sh
+++ export.sh
@@ -7,13 +7,13 @@ else
 fi
 
 # Portraits
-$ASEPRITE -b ./assets/portraits.aseprite --layer Frame --scale 4 --save-as ./assets/portrait_frame.png
-$ASEPRITE -b ./assets/portraits.aseprite --layer Rogue --scale 4 --save-as ./assets/portrait_rogue.png
-$ASEPRITE -b ./assets/portraits.aseprite --layer Wizard --scale 4 --save-as ./assets/portrait_wizard.png
+$ASEPRITE -b ./assets/ui/portraits.aseprite --layer Frame --scale 4 --save-as ./assets/ui/portrait_frame.png
+$ASEPRITE -b ./assets/ui/portraits.aseprite --layer Rogue --scale 4 --save-as ./assets/ui/portrait_rogue.png
+$ASEPRITE -b ./assets/ui/portraits.aseprite --layer Wizard --scale 4 --save-as ./assets/ui/portrait_wizard.png
 
 # Other UI elements
-$ASEPRITE -b ./assets/action_border.aseprite --layer Wizard --scale 4 --save-as ./assets/action_border.png
-$ASEPRITE -b ./assets/switch_button.aseprite --frame-range 0,0 --scale 4 --save-as ./assets/switch_button.png
-$ASEPRITE -b ./assets/switch_button.aseprite --frame-range 1,1 --scale 4 --save-as ./assets/switch_button_pressed.png
+$ASEPRITE -b ./assets/ui/action_border.aseprite --layer Wizard --scale 4 --save-as ./assets/ui/action_border.png
+$ASEPRITE -b ./assets/ui/switch_button.aseprite --frame-range 0,0 --scale 4 --save-as ./assets/ui/switch_button.png
+$ASEPRITE -b ./assets/ui/switch_button.aseprite --frame-range 1,1 --scale 4 --save-as ./assets/ui/switch_button_pressed.png
 
 echo "Assets exported successfully <3"
